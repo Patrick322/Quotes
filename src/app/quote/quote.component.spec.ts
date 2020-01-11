bc776110ea175a126bcc32288fa4339ac7532f6f
Patrick595 <patrickbuong@gmail.com>
2020-01-11 14:27:01 +0300

    it will change the structure of the application

--- src/app/quote/quote.component.spec.ts
+++ src/app/quote/quote.component.spec.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
-import { Quote } from './quote';
+import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote';
+
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-goal',
+  templateUrl: './goal.component.html',
+  styleUrls: ['./goal.component.css']
 })
-export class AppComponent {
+export class GoalComponent implements OnInit {
+
   quotes:Quote [] = [
     {id:1, name:'Live as if you were to die tomorrow. Learn as if you were to live forever'},
     {id:2,name:'That which does not kill us makes us stronger'},
@@ -13,4 +15,10 @@ export class AppComponent {
     {id:4,name:'Do what you can, with what you have, where you are.”'},
     {id:5,name:'If opportunity doesn’t knock, build a doo'},
   ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
 }
