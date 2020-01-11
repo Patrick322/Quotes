2e4065af3f65d6848f3565b854b35144908ac6eb
Patrick595 <patrickbuong@gmail.com>
2020-01-11 19:33:22 +0300

    modifie the application

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -18,15 +18,10 @@ export class QuoteComponent implements OnInit {
   ];
 
 
-  completeQuote(isComplete, index){
-    if (isComplete) {
-      this.quotes.splice(index,1);
-    }
+  constructor() { }
+
+  ngOnInit() {
+
   }
   
-  constructor() { }
- 
-  ngOnInit() {
-  }
-
 }
