1ec0fe9f52b54746c6779e895a157ff81cb2c667
Patrick595 <patrickbuong@gmail.com>
2020-01-11 15:14:42 +0300

    it will change the structure of the application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,5 @@ import { Quote } from './quote';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  quotes:Quote [] = [
-    {id:1, name:'Live as if you were to die tomorrow. Learn as if you were to live forever'},
-    {id:2,name:'That which does not kill us makes us stronger'},
-    {id:3,name:'Be yourself; everyone else is already taken'},
-    {id:4,name:'Do what you can, with what you have, where you are.”'},
-    {id:5,name:'If opportunity doesn’t knock, build a doo'},
-  ];
+
 }
--- src/app/quote/quote.component.spec.ts
+++ src/app/quote/quote.component.spec.ts
@@ -2,19 +2,21 @@ import { Component, OnInit } from '@angular/core';
 import { Quote } from '../quote';
 
 @Component({
-  selector: 'app-goal',
-  templateUrl: './goal.component.html',
-  styleUrls: ['./goal.component.css']
+  selector: 'app-quote',
+  templateUrl: './quote.component.html',
+  styleUrls: ['./quote.component.css']
 })
 export class GoalComponent implements OnInit {
 
-  quotes:Quote [] = [
-    {id:1, name:'Live as if you were to die tomorrow. Learn as if you were to live forever'},
-    {id:2,name:'That which does not kill us makes us stronger'},
-    {id:3,name:'Be yourself; everyone else is already taken'},
-    {id:4,name:'Do what you can, with what you have, where you are.”'},
-    {id:5,name:'If opportunity doesn’t knock, build a doo'},
+  goals: Quote[] = [
+    new Quote(1, 'Watch finding Nemo', 'Find an online version and watch merlin find his son'),
+    new Quote(2,'Buy Cookies','I have to buy cookies for the parrot'),
+    new Quote(3,'Get new Phone Case','Diana has her birthday coming up soon'),
+    new Quote(4,'Get Dog Food','Pupper likes expensive snacks'),
+    new Quote(5,'Solve math homework','Damn Math'),
+    new Quote(6,'Plot my world domination plan','Cause I am an evil overlord'),
   ];
+
 
   constructor() { }
 
